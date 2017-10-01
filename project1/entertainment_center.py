ae0ef2555188593c21c9f6d4639697a6812612f8
Basma Ashour <basmaashouur@gmail.com>
2017-10-01 17:37:15 +0200

    Used csv for the movies data

--- project1/entertainment_center.py
+++ project1/entertainment_center.py
@@ -1,42 +1,16 @@
 import media
 import fresh_tomatoes
+import csv
 
 
 def get_movies():
-    """ function make an instance from Movie class to represent a movie """
-    imitation_game = media.Movie(
-                           "The Imitation Game",
-                           "Story about Alan Turing to crack Nazi codes.",
-                           "https://goo.gl/C3Jatc",
-                           "https://www.youtube.com/watch?v=S5CjKEFb-sM&t=32s")
-
-    storyof_evrything = media.Movie(
-                            "The Theory of Everything",
-                            "Story about the physicist Stephen Hawking.",
-                            "https://goo.gl/PDuwsm",
-                            "https://www.youtube.com/watch?v=Salz7uGp72c&t=3s")
-    knew_inf = media.Movie(
-                    "The Man Who Knew Infinity",
-                    "Story about Ramanujan travels to Trinity College.",
-                    "https://goo.gl/79Qmot",
-                    "https://www.youtube.com/watch?v=oXGm9Vlfx4w&t=16s")
-    hidden_figures = media.Movie(
-                           "Hidden Figures",
-                           "Story about brilliant African-American women.",
-                           "https://goo.gl/s18NQC",
-                           "https://www.youtube.com/watch?v=5wfrDhgUMGI")          
-    beautiful_mind = media.Movie(
-                           "A Beautiful Mind",
-                           "Story about the mathematical John Forbes Nas.",
-                           "https://goo.gl/HPQuUm",
-                           "https://www.youtube.com/watch?v=aS_d0Ayjw4o")
-    concussion = media.Movie(
-                       "Concussion",
-                       "Story about Dr. Bennet Omalu.",
-                       "https://goo.gl/fYDy2W",
-                       "https://www.youtube.com/watch?v=LVRIntuk7uc")
-    movies = [imitation_game, storyof_evrything,
-              knew_inf, beautiful_mind, hidden_figures, concussion]
+    """ make an instance from Movie class and put """
+    """ the movie data from cvc file to represent a movie"""
+    movies = []
+    with open('names.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for line in csv_reader:
+            movies.append(media.Movie(line[0], line[1], line[2], line[3]))
     return movies
 
 
