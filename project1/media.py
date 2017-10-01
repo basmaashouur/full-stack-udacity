c92f9449b193cf8137ab307e323a9f4b6dfa495e
Basma Ashour <basmaashouur@gmail.com>
2017-10-01 14:57:04 +0200

    First commit

---
+++ project1/media.py
@@ -0,0 +1,8 @@
+class Movie():
+    def __init__(
+                self, title, movie_storyline,
+                poster_image_url, trailer_youtube_url):
+                    self.title = title
+                    self.movie_storyline = movie_storyline
+                    self.poster_image_url = poster_image_url
+                    self.trailer_youtube_url = trailer_youtube_url
